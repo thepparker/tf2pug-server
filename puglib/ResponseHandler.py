e5f81517cd9e505beb4076c414dce26f5b350a54
Prithu Parker <prithu@iinet.net.au>
2013-12-30 22:20:06 +1100

    added more response codes, changed method names to be shorter and simpler

--- puglib/ResponseHandler.py
+++ puglib/ResponseHandler.py
@@ -4,17 +4,67 @@
 Response_PugListing = 1000
 Response_PugStatus = 1001
 Response_InvalidPug = 1002
+Response_PugEnded = 1003
+Response_PugFull = 1004
+Response_EmptyPugEnded = 1005
 
-Response_PlayerList = 1003
+Response_PlayerList = 1100
+Response_PlayerInPug = 1101
+Response_PlayerNotInPug = 1102
+Response_PlayerAdded = 1103
+Response_PlayerRemoved = 1104
 
-Response_MapVoteAdded = 1010
+Response_MapVoteAdded = 1200
 
 class ResponseHandler(object):
     def __init__(self):
         pass
 
-    def construct_player_list_packet(self, pug):
+    def change_response_code(self, packet, new_code):
+        packet["response"] = new_code
+
+        return packet
+
+    def player_added(self, pug):
+        response = self.pug_status(pug)
+
+        self.change_response_code(response, Response_PlayerAdded)
+
+        return response
+
+    def player_removed(self, pug):
+        response = self.pug_status(pug)
+
+        self.change_response_code(response, Response_PlayerRemoved)
+
+        return response
+
+    def player_in_pug(self, pug):
+        response = self.pug_status(pug)
+
+        self.change_response_code(response, Response_PlayerInPug)
+
+        return response
+
+    def player_not_in_pug(self, pug):
+        return { "response": Response_PlayerNotInPug }
+
+    def pug_full(self, pug):
+        response = self.pug_status(pug)
+
+        self.change_response_code(response, Response_PugFull)
+
+        return response
+
+    def invalid_pug(self):
+        return { "response": Response_InvalidPug }
+
+    def empty_pug_ended(self):
+        return { "response": Response_EmptyPugEnded }
+
+    def player_list(self, pug):
         response = {}
+
         if pug is None:
             response["response"] = Response_InvalidPug
         else:
@@ -25,7 +75,7 @@ class ResponseHandler(object):
 
         return response
 
-    def construct_list_packet(self, pugs):
+    def pug_listing(self, pugs):
         response = {
             "response": Response_PugListing
 
@@ -34,7 +84,7 @@ class ResponseHandler(object):
 
         return response
 
-    def construct_status_packet(self, pug):
+    def pug_status(self, pug):
         response = {}
 
         if pug is None:

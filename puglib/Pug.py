db3ef1da3960d48453b692802fce33d9d96249c5
Prithu Parker <prithu@iinet.net.au>
2014-02-06 21:51:07 +1100

    team generation is well on the way!

--- puglib/Pug.py
+++ puglib/Pug.py
@@ -2,6 +2,8 @@
 import collections
 import time
 import calendar
+import random
+import logging
 
 states = {
     "GATHERING_PLAYERS": 0,
@@ -34,6 +36,7 @@ class Pug(object):
             self.map_forced = False
 
         self.admin = None
+        #players is a dict in the form { cid: "name", ... }
         self._players = collections.OrderedDict()
 
         self.player_votes = {}
@@ -134,7 +137,98 @@ class Pug(object):
         if not self.full:
             return
 
-        # HOW TO GENERATE TEAMS???
+        # To select teams, we have to first select a medic for each team. 
+        # After that we need to establish a score for each player and sort 
+        # them by it
+
+        # firstly make sure every player has a key in the stat data. if any
+        # are missing, we'll give them blank stats
+        for player in self._players:
+            if player not in stat_data:
+                stat_data[player] = {
+                    "games_since_med": 0
+                    "games_played": 0,
+                    "kills": 0,
+                    "assists": 0,
+                    "deaths": 0,
+                    "damage_dealt": 0,
+                    "numplayed": 0
+                }
+        
+        # to select a medic, we loop over the players in the pug, picking
+        # whoever has played more games since last playing medic until we have
+        # atleast 2 potential medics. then we randomly pick 2 from the list
+
+        potential_medics = []
+        threshold = 4
+        # need atleast 2 potential medics
+        while len(potential_medics) < 2:
+            # add players who have played more games than the threshold until
+            # we have atleast 2 players. if we don't get 2 players on the
+            # first run, the threshold is decreased to incorporate more players
+            # until eventually the threshold may be 0 and every player is a
+            # candidate
+            for cid in self._players:
+                if stat_data[cid]["games_since_med"] > threshold and cid not in potential_medics:
+                    potential_medics.append(cid)
+
+            threshold -= 1
+
+        # now we have our potential medics, so we can just shuffle and select them
+        random.shuffle(potential_medics)
+        logging.debug("Potential medics shuffled: %s", potential_medics)
+
+        self.medic_blue = potential_medics.pop()
+        self.medic_red = potential_medics.pop()
+
+        # now that we have our medics, we can remove them from the player stats
+        # & calculate player scores
+        del stat_data[self.medic_blue]
+        del stat_data[self.medic_red]
+
+        # add player scores to the stat data
+        total_pr = 0
+        for cid in stat_data:
+            score = self._player_score(stat_data[cid])
+            stat_data[cid]["score"] = score
+
+            total_pr += score
+            logging.debug("PLAYER: %s, SCORE: %s. New total: %s", cid, score, total_pr)
+
+        # now we can sort by score. we'll get a list of sorted ids out
+        # in order of highest score to lowest
+        sorted_ids = sorted(stat_data, key = lambda k: stat_data[k]["score"], reverse = True)
+
+        # now just setup the teams. SIMPLE, RIGHT? WRONG
+        self.team_red.append(self.medic_red)
+        self.team_blue.append(self.medic_blue)
+
+        self.__allocate_players(total_pr, sorted_ids)
+
+    def __allocate_players(self, total_pr, ids):
+        # each team needs to have approximately total_pr/2 skill rating, or
+        # as close to this as possible, in order to be considered even.
+        # therefore, we need to iterate over possible team combinations
+        # until we have the most even
+
+        avg_rating = total_pr/2
+
+
+    def _player_score(self, pdata):
+        # we need to establish a score for each player. we'll call this the 
+        # 'pug rating', or PR
+        # the formula is as follows:
+        #   (100 + (kills + assists)/deaths + (damage_dealt/1000)/numplayed)^1.2
+        score = 100
+            
+        if pdata["numplayed"] > 0:
+            score += (pdata["kills"] + pdata["assists"]) / (pdata["deaths"] if pdata["deaths"] else 1)
+            score += (pdata["damage_dealt"] / 1000) / pdata["numplayed"]
+
+        score **= 1.2
+
+        return score
+
 
     def has_player(self, player_id):
         return player_id in self._players

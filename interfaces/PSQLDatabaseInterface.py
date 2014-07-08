2adbb3f7df56e4aa79b9e17620296e2c4e68c35b
Prithu Parker <prithu@iinet.net.au>
2014-07-08 21:01:40 +1000

    implemented get_player_stats and flush_pug_med_stats, made get_user_info uniform, begun get_pugs()

--- interfaces/PSQLDatabaseInterface.py
+++ interfaces/PSQLDatabaseInterface.py
@@ -14,15 +14,134 @@ class PSQLDatabaseInterface(BaseDatabaseInterface):
 
             if api_key:
                 cursor.execute("SELECT name, group FROM api_keys WHERE key = %s", (api_key,))
-                result = cursor.fetchone()
+
             else:
                 cursor.execute("SELECT name, group, key FROM api_keys")
-                result = cursor.fetchall()
+            
+            result = cursor.fetchall()
 
             return result
 
         except:
             logging.exception("An exception occurred getting user info")
+
+        finally:
+            self._close_db_objects(cursor, conn)
+
+    def get_player_stats(self, ids):
+        conn, cursor = self._get_db_objects()
+
+        # ids is a list of 64 bit steamids (i.e pug.players_list)
+        try:
+            cursor.execute("""SELECT steamid, games_since_med, games_played, rating
+                              FROM players
+                              WHERE steamid IN %s""", (tuple(ids),))
+
+            results = cursor.fetchall()
+            stats = {}
+
+            if results:
+                for result in results:
+                    logging.debug("player stat row: %s" % result)
+
+                    stats[result["steamid"]] = { 
+                            "games_since_med": result["games_since_med"],
+                            "games_played": result["games_played"],
+                            "rating": result["rating"]
+                        }
+
+            return stats
+
+        except:
+            logging.exception("An exception occurred getting stats for %s" % ids)
+
+        finally:
+            self._close_db_objects(cursor, conn)
+
+    def flush_pug_med_stats(self, medics, nonmedics):
+        conn, cursor = self._get_db_objects()
+
+        try:
+            # mogrifying inserts. we need to check who is NOT in the database.
+            # to do this, we use the given list of IDs, get the results, and
+            # for any IDs NOT in the result, we perform an insert.
+            # we do this so that we minimize the number of queries required, so
+            # that worst case is O(n+2) instead of ALWAYS being O(n+2) when 
+            # inserts are required. If everyone is being updated, efficiency is
+            # O(n+2). It also removes the need for using the dirty upsert 
+            # function.
+
+            # non medics first
+            cursor.execute("SELECT steamid FROM players WHERE steamd IN %s", (tuple(nonmedics),))
+            results = cursor.fetchall()
+            results = [ x[0] for x in results ] # make results a simple list of IDs
+
+            # now get all ids in nonmedics that are NOT in results
+            insert_ids = [ x for x in nonmedics if x not in results ]
+            if len(insert_ids) > 0: # obviously only perform if we have ids to insert...
+                # create the list of tuples for insertion
+                data = tuple([ (x, 1, 1) for x in insert_ids ])
+
+                cursor.executemany("""INSERT INTO players (steamid, games_since_med, games_played)
+                                      VALUES ('%s', '%s', '%s')""", data)
+
+            #the rest, we just update
+            update_ids = [ x for x in nonmedics if x not in insert_ids ]
+            for cid in update_ids:
+                cursor.execute("""UPDATE players
+                                  SET games_since_med = COALESCE(games_since_med, 0) + 1,
+                                      games_played = COALESCE(games_played, 0) + 1
+                                  WHERE steamid = '%s'""", (cid,))
+            conn.commit()
+
+            # now medics, do the same as we did for non-medics
+            cursor.execute("SELECT steamid FROM players WHERE steamid IN %s", (tuple(medics),))
+            results = cursor.fetchall()
+            results = [ x[0] for x in results ]
+
+            insert_ids = [ x for x in medics if x not in results ]
+            if len(insert_ids) > 0:
+                data = tuple([ (x, 0, 1) for x in insert_ids ])
+
+                cursor.executemany("""INSERT INTO players (steamid, games_since_med, games_played)
+                                  VALUES ('%s', '%s', '%s')""", data)
+
+            update_ids = [ x for x in medics if x not in insert_ids ]
+            for cid in update_ids:
+                cursor.execute("""UPDATE players
+                                  SET games_since_med = 0,
+                                      games_played = COALESCE(games_played, 0) + 1
+                                  WHERE steamid = '%s'""", (cid,))
+
+            conn.commit()
+
+        except:
+            logging.exception("An exception occurred flushing med stats")
+
+        finally:
+            self._close_db_objects()
+
+    def get_pugs(self, api_key, include_finished = False):
+        conn, cursor = self._get_db_objects()
+
+        try:
+            # first we get the pug ids we're after from the index table
+            cursor.execute("""SELECT pug_entity_id
+                              FROM pugs_index
+                              WHERE api_key = %s AND finished = %s""",
+                              (api_key, finished))
+            pug_ids = [ x[0] for x in cursor.fetchall() ] # results is a list of entity ids
+
+            cursor.execute("""SELECT id, data
+                              FROM pugs
+                              WHERE id IN %s""", (tuple(pug_ids),))
+
+            results = cursor.fetchall() 
+            # results is a list of tuples containing (id, pug data as JSON)
+
+
+        except:
+            logging.exception("An exception occurred getting pug data")
 
         finally:
             self._close_db_objects(cursor, conn)

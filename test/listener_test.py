1d62e618b05adba75a571ff2eec9b55792eb2910
Prithu Parker <prithu@iinet.net.au>
2014-07-10 02:14:11 +1000

    update zee listener test

--- test/listener_test.py
+++ test/listener_test.py
@@ -2,12 +2,17 @@ import UDPServer
 from interfaces import TFLogInterface
 #from entities import Server
 from tornado import ioloop
+import settings
+import logging
+
+import tornado.options
 
 server_address = (settings.listen_ip, 0)
+print server_address
 iface = TFLogInterface.TFLogInterface(None)
 
-listener = UDPServer(server_address, iface.parse)
+listener = UDPServer.UDPServer(server_address, iface.parse)
 
 listener.start()
 
-ioloop.IOLoop.instance.start()
+ioloop.IOLoop.instance().start()

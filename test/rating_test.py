25c536b99383a94cef2a750adbbfd1cd42da8b0f
Prithu Parker <prithu@iinet.net.au>
2014-07-16 00:38:33 +1000

    made ratings all a single tier in rate_4v4, added addition/subtraction unit test

--- test/rating_test.py
+++ test/rating_test.py
@@ -8,11 +8,16 @@ def test_rating():
     assert Rating(4) > Rating(3)
     assert Rating(3) < Rating(4)
 
+    assert Rating(2) + Rating(2) == Rating(4)
+    assert Rating(4) - Rating(2) == Rating(2)
+
 def rate_4v4():
-    team1 = [ Rating(1530+x) for x in range(4) ]
-    team2 = [ Rating(1460+20*x) for x in range(4) ]
+    team1 = [ Rating(1570+x) for x in range(4) ]
+    team2 = [ Rating(1500+20*x) for x in range(4) ]
     
+    print (team1,team2)
     print calculate_rating([team1, team2], [0, 1])
 
+test_rating()
 rate_4v4()
 

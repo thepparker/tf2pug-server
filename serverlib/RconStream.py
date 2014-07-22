839af191149b36bf0574458f2b3964c3104e6856
Prithu Parker <prithu@iinet.net.au>
2014-07-22 22:26:28 +1000

    gutted most of the old code. RconStream will now be async only. If synchronous RCON is desired, use the other library. trying to mesh them together is a recipe for a migraine

--- serverlib/RconStream.py
+++ serverlib/RconStream.py
@@ -8,6 +8,9 @@ import struct
 import logging
 
 from tornado.iostream import IOStream
+from tornado import gen
+
+from functools import partial
 
 SERVERDATA_AUTH = 3
 SERVERDATA_EXEC_COMMAND = 2
@@ -43,7 +46,7 @@ class RconConnectionInterruptedError(Exception):
     pass
 
 class RconConnection(object):
-    def __init__(self, ip, port, rcon_password, command):
+    def __init__(self, ip, port, rcon_password):
         self.ip = ip
         self.port = port
         self.rcon_password = rcon_password
@@ -54,18 +57,8 @@ class RconConnection(object):
         self.authed = False
         self.request_id = 0
 
-        # connect and immediately call _auth
+        # async connect & call _auth when connected
         self._stream.connect((ip, port), self._auth)
-
-    def _connect(self):
-        if self._socket:
-            try:
-                self._socket.connect((self.ip, self.port))
-            except:
-                self.close()
-                raise RconConnectionError("Unable to connect to server")
-        else:
-            raise RconConnectionError("Cannot connect to dead socket")
 
     def _construct_packet(self, code, body):
         #if len(body) > 510:
@@ -90,196 +83,76 @@ class RconConnection(object):
 
         return packet
 
-    def _read_response(self, callback):
-        """
-        Get the response from an rcon command
-        """
+    def _send_packet(self, packet, callback = None):
+        return self._steam.write(packet, callback)
 
-        # First step is the first packet length.
-        packet_len_packed = self.__get_packet_len()
+    def _begin_read_packet(self):
+        packet_len_packed = yield self._stream.read_bytes(4)
         packet_len = struct.unpack('<l', packet_len_packed)[0]
 
-        # now we read `packet_len` size from the socket, feeding the data
-        # into our internal callback
-        self._read_stream(packet_len, self._internal_receive_data)
+        return self._read_packet(packet_len)
 
-        # The rest is up to the internal callback?
-        #if callback is None:
-        #    yield SOMETHING
-         
+    def _read_packet(self, numbytes):
+        # we have the packet length. now we want to read LENGTH bytes from the
+        # socket. If we read async, we'll need to have it pass the result
+        # through our callback function, which will then process the packet and
+        # pass it onto the given callback.
+        # If we blocking read, we need to pass that data to our processing
+        # method, which will return the data
 
-    def _internal_receive_data(self, data):
-        """
-        This is an internal callback, and will take data as read by the stream,
-        compiling it until the response is complete. Once we have the complete
-        response, either a callback is run, or the data is yielded.
-        """
+        # let's implement blocking first, then figure out how to implement it
+        # async
+        packet_packed = yield self._stream.read_bytes(numbytes)
 
-        first_packet_id = None #this will be the ID return in multi-line responses
-        curr_packet_id = 0 #this will be the ID checked against for the end of multi-line responses
-        response_code = 0
-        entire_message = ''
+        #packet is in the form <id (packed int)><response code (packed int)><body>\x00\x00
+        curr_packet_id = struct.unpack('<l', packet_packed[0:4])[0]
+        response_code = struct.unpack('<l', packet_packed[4:8])[0]
+        message = packet_packed[8:].strip('\x00') #strip the terminators
 
-        got_empty_packet = False
+        # we now have the packet message, response code, and response id. 
+        # if this packet is an auth packet, we can just return here. else, it
+        # _COULD_ be a multi-line response, in which case we need to keep
+        # reading until we've hit the end. The end is signified by an empty
+        # packet after a mirror packet.
 
-        while 1:
-            packet_len_packed = self.__get_packet_len()
-            # now we have the entire packet length. 
-            # all data is not necessarily sent in a single packet, and may be 
-            # split into multiple packets
-            packet_len = struct.unpack('<l', packet_len_packed)[0] 
-
-            packet_packed = self.__read_socket(packet_len)
-
-            logging.debug("Entire packet: %s", repr(packet_len_packed + packet_packed))
-
-            #packet is in the form <id (packed int)><response code (packed int)><body>\x00\x00
-            curr_packet_id = struct.unpack('<l', packet_packed[0:4])[0]
-            response_code = struct.unpack('<l', packet_packed[4:8])[0]
-            message = packet_packed[8:].strip('\x00') #strip the terminators
-
-            if first_packet_id is None:
-                first_packet_id = curr_packet_id
-
-            if not self.authed:
-                #we're waiting for an auth packet, so we should just return from here for now
-                return (curr_packet_id, response_code, message)
-
-            else:
-                if response_code == SERVERDATA_COMMAND_RESPONSE and curr_packet_id == self.request_id:
-                    #this is the mirror packet after sending a RESPONSE_VALUE command, so we know our potentially multi-line response has ended
-                    if got_empty_packet:
-                        logging.debug("Got empty packet after mirror. This signals end of multi-line response")
-                        return (first_packet_id, response_code, entire_message)
-                    
-                    else:
-                        logging.debug("Got empty mirror response")
-                        got_empty_packet = True
-                
-
-                entire_message += message #concatenate the read message onto the full message
-
-
-
-    def _run_callback(self):
-        # we need to process the data received, then push it through the 
-        # callback (if set?)
-
-    def _read_stream(self, numbytes, callback):
-        self._stream.read_bytes(numbytes, callback)
-
-    def _read_stream_blocking(self, numbytes):
-        data = self._stream.read_bytes(numbytes)
-        yield data.result()
-
-    def __get_packet_len(self):
-        """
-        Reads the packet length from the stream and yields it. This is the only
-        read operation which will ALWAYS block.
-
-        The first 4 bytes in a packet are the packet length
-        """
-        return self._read_stream_blocking(4)
-
-    def __read_socket(self, num_bytes):
-        #loops over the socket, reading until num_bytes is read
-        if self.closed:
-            raise RconConnectionError("Cannot read from closed socket")
-
-        data = r''
-        num_read = 0
-
-
-
-        while num_read < num_bytes:
-            try:
-                rcvd = self._socket.recv(num_bytes - num_read)
-            except:
-                raise RconConnectionError("Connection timed out while waiting for data")
-
-            if len(rcvd) == 0:
-                raise RconConnectionInterruptedError("RCon connection unexpectedly closed")
-
-            data += rcvd
-
-            num_read = len(data)
-
-        logging.debug("Data read: %s", to_hex(data))
-        return data
-
-    def _send_packet(self, packet):
-        if self._stream and not self._stream.closed():
-            logging.debug("Packet to send: %s", repr(packet))
-            self._stream.write(packet)
+        if not self._authed:
+            # just return here
+            return (curr_packet_id, response_code, message)
 
         else:
-            raise RconConnectionError("Cannot send to dead socket")
-
+            # possible multi line response.
+            pass
+        
 
     def _auth(self):
-        if not self.authed:
-            auth_packet = self._construct_packet(SERVERDATA_AUTH, self.rcon_password)
-            self._send_packet(auth_packet)
+        """
+        Called when connect is successful. First thing we always do after
+        connecting is attempt to authenticate. Authentication is asynchronous.
+        We'll utilise partials to do this properly
+        """
+        auth_packet = self._construct_packet(SERVERDATA_AUTH, self.rcon_password)
 
-            junk = self._receive_data() # get junk packet pre-auth
-            response = self._receive_data()
+        # _auth will be called again once the auth packet has been sent
+        self._send_packet(auth_packet, self._auth)
 
-            logging.debug("Auth response: (junk): %s, (auth): %s", repr(junk), repr(response))
+        junk = yield self._begin_read_packet()
+        # now just get real response
+        response = yield self._begin_read_packet()
 
-            if response[1] == SERVERDATA_AUTH_RESPONSE:
-                if response[0] == -1:
-                    raise RconAuthError("Invalid RCON password specified")
+        if response[1] == SERVERDATA_AUTH_RESPONSE:
+            if response[0] == -1:
+                raise RconAuthError("Invalid RCON password specified")
 
-                elif response[0] == self.request_id:
-                    self.authed = True
+            elif response[0] == self.request_id:
+                self.authed = True
 
-                    logging.debug("Successfully authed")
-
-                else:
-                    raise RconAuthError("Unknown packet id (%d) received when auth packet was expected with id %d" % (response[0], self.request_id))
+                logging.debug("Successfully authed")
 
             else:
-                raise RconAuthError("Received unexpected response code when auth response was expected")
-
-    def _exec(self, cmd):
-        if self.authed:
-            cmd_packet = self._construct_packet(SERVERDATA_EXEC_COMMAND, cmd)
-
-            self._send_packet(cmd_packet)
-            # send a "response" packet, which srcds will mirror back after 
-            # responding to the EXEC, letting us know the exec command has 
-            # finished. this makes it easier to read multi-line responses
-            self._send_packet(self._construct_packet(SERVERDATA_COMMAND_RESPONSE, r''))
-
-            return self._receive_data()
+                raise RconAuthError("Unknown packet id (%d) received when auth packet was expected with id %d" % (response[0], self.request_id))
 
         else:
-            raise RconError("RCon connected is not authed. Cannot send command")
-
-    def send_cmd(self, cmd, callback = None):
-        """
-        Support a callback, which the rcon response will be parsed through. If no
-        callback is given, we yield the data from _receive_data via a future
-        """
-        if not self.authed:
-            self._auth()
-
-        result = self._exec(cmd)
-
-        if self.run_once:
-            logging.debug("Single command executed. Closing socket")
-            self.close()
-
-        return result
-
-    def close(self):
-        if self._socket and not self.closed:
-            self._socket.close()
-
-            self.closed = True
-
-    def fileno(self):
-        return self._socket.fileno()
+            raise RconAuthError("Received unexpected response code when auth response was expected")
 
     @property
     def closed(self):

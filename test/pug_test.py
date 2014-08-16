30f2d71ddd444fe9fb13b504f3f1e4010a251b8c
Prithu Parker <prithu@iinet.net.au>
2014-08-16 19:15:58 +1000

    Partial unit test for the Pug class

---
+++ test/pug_test.py
@@ -0,0 +1,189 @@
+"""
+Test case for pug methods such as shuffle_teams, update_end_stats, map voting,
+player restriction, updating game stats, etc.
+"""
+
+import sys
+import logging
+sys.path.append('..')
+logging.basicConfig(level=logging.DEBUG)
+
+from entities import Pug
+from entities.Pug import Pug, PlayerStats
+
+def test_add_player():
+    pug = Pug()
+    pug.add_player(1L, "1", PlayerStats())
+    assert 1L in pug._players
+    assert 1L in pug.player_stats
+    assert 1L in pug.game_stats
+
+def test_remove_player():
+    pug = Pug()
+    pug.add_player(1L, "1", PlayerStats())
+
+    # we need to make sure the map vote is removed too
+    pug.begin_map_vote()
+    pug.vote_map(1L, "cp_badlands")
+
+    pug.remove_player(1L)
+    assert 1L not in pug._players
+    assert 1L not in pug.player_stats
+    assert 1L not in pug.game_stats
+    assert 1L not in pug.player_votes
+
+def test_add_to_team():
+    pug = Pug()
+    pug.__add_to_team("blue", 1L)
+    assert pug.teams["blue"] == set([ 1L ])
+    pug.__add_to_team("blue", [2L, 3L])
+    assert pug.teams["blue"] == set([ 1L, 2L, 3L ])
+
+def test_remove_from_team():
+    pug = Pug()
+    pug.__add_to_team("blue", [ 1L, 2L, 3L ])
+    
+    pug.__remove_from_team("blue", [ 1L ])
+    assert pug.teams["blue"] == set([ 2L, 3L ])
+
+    pug.__remove_from_team("blue", [ 2L, 3L ])
+    assert pug.teams["blue"] == set([])
+
+def fill_pug():
+    pug = Pug()
+    pid = 1L
+    while not pug.full:
+        pug.add_player(pid, str(pid), PlayerStats())
+
+    assert pug.full == True
+
+    return pug
+
+def test_shuffle_teams():
+    pug = fill_pug()
+
+    pug.shuffle_teams()
+
+def test_map_vote():
+    print "Testing map vote with player vote"
+    pug = Pug()
+    pug.add_player(1, str(1), PlayerStats())
+
+    pug.begin_map_vote()
+    assert pug.state == Pug.states["MAP_VOTING"]
+    last_map = ""
+    for m in pug.maps: 
+        pug.vote_map(1, m)
+        assert pug.player_votes[1] == m
+        assert len(pug.map_votes) > 0
+        assert pug.map_votes.most_common(1)[0] == (m, 1)
+        last_map = m
+
+    pug.end_map_vote()
+    assert pug.state == Pug.states["MAPVOTE_COMPLETED"]
+    assert pug.map == last_map
+
+    #------------------------------------------------------
+    print "Testing map vote with no vote"
+    pug = Pug()
+    pug.begin_map_vote()
+    assert pug.state == Pug.states["MAP_VOTING"]
+    pug.end_map_vote()
+    assert pug.state == Pug.states["MAPVOTE_COMPLETED"]
+    assert pug.map is not None
+
+    #------------------------------------------------------
+    print "Testing map vote with forced map"
+    pug = Pug()
+    pug.force_map("cp_granary")
+    pug.begin_map_vote()
+    assert pug.state == Pug.states["MAPVOTE_COMPLETED"]
+    assert pug.map == "cp_granary"
+
+    pug = Pug()
+    pug.begin_map_vote()
+    assert pug.state == Pug.states["MAP_VOTING"]
+    
+    pug.force_map("cp_granary")
+    assert pug.state == Pug.states["MAPVOTE_COMPLETED"]
+    assert pug.map == "cp_granary"
+
+def test_update_score():
+    pug = Pug()
+    assert pug.game_scores["blue"] == pug.game_scores["red"] == 0
+
+    pug.update_score("blue", 2)
+    assert pug.game_scores["blue"] == 2
+    assert pug.game_scores["red"] == 0
+
+    pug.update_score("red", 3)
+    assert pug.game_scores["blue"] == 2
+    assert pug.game_scores["red"] == 3    
+
+def test_player_restriction():
+    pug = Pug()
+    print "Testing player restriction. Min rating: 100"
+    pug.player_restriction = 100
+
+    assert pug.player_restricted(120) == False
+    assert pug.player_restricted(100) == False
+    assert pug.player_restricted(90) == True
+
+    pug.player_restriction = -100
+    assert pug.player_restricted(100) == True
+    assert pug.player_restricted(120) == True
+    assert pug.player_restricted(90) == False
+
+def test_helpers():
+    pug = Pug()
+    pug.add_player(1L, "1", PlayerStats())
+
+    assert pug.has_player(1L) == True
+    assert pug.player_list() == [ 1L ]
+    assert pug.is_admin(1L) == True
+    
+    assert pug.player_role(1L) is None
+    pug.medics["blue"] = 1L
+    assert pug.player_role(1L) == "Medic"
+
+    assert pug.player_name(1L) == "1"
+    assert pug.get_state_string() == "GATHERING_PLAYERS"
+
+    assert pug.teams_done == False
+    assert pug.full == False
+    assert pug.player_count == 1
+    assert pug.game_started == False
+    assert pug.game_over == False
+    assert pug.password is None
+    assert len(pug.map_votes) == 0
+
+def test_update_game_stats():
+    pug = Pug()
+    pug.add_player(1L, "1", PlayerStats())
+    
+    assert pug.game_stats[1L] == PlayerStats()
+    assert pug.game_stats[1L] is pug._get_player_stats(1L)
+
+    assert pug.game_stats[1L]["kills"] == 0
+
+    pug.update_game_stat(1L, "kills", 1)
+    assert pug.game_stats[1L]["kills"] == 1
+
+    pug.update_game_stat(1L, "kills", 1)
+    assert pug.game_stats[1L]["kills"] == 2
+
+    pug.update_game_stat(1L, "kills", 10, increment = False)
+    assert pug.game_stats[1L]["kills"] == 10
+    
+
+def test_update_end_stats():
+    pug = fill_pug()
+
+    # add stats to game stats and our own stat dict, merge, then assert
+    # they are equal afterwards. fuark
+
+def test():
+    
+    
+
+test()

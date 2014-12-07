3592edbb07f515f95a1124774f11dec9186faaa8
Prithu Parker <prithu@iinet.net.au>
2014-12-07 17:52:24 +1100

    Added gravel and snakewater to available maps

--- entities/Pug.py
+++ entities/Pug.py
@@ -17,7 +17,7 @@ states = {
 }
 
 MAPVOTE_DURATION = 1 # Time in seconds for map vote duration
-AVAILABLE_MAPS = [ u"cp_granary", u"cp_badlands" ]
+AVAILABLE_MAPS = [ u"cp_granary", u"cp_badlands", "cp_gravelpit", "cp_snakewater" ]
 REPLACE_TIMEOUT = 10 # Time in seconds to wait for a replace
 DISCONNECT_TIMEOUT = 100 # Time in seconds to wait before replacing a disconnect
 
